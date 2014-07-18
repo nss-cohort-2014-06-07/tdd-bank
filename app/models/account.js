eea00537409a0ba46f72db593cc9bc1206263a5a
Chyld Medford <chyld.medford@gmail.com>
2014-07-18 14:58:29 -0500

    withdraws

--- app/models/account.js
+++ app/models/account.js
@@ -5,13 +5,36 @@ function Account(number, name, deposit, type){
   this.name = name;
   this.type = type;
   this.balance = deposit;
+  this.isSuspended = false;
   this.deposits = [];
   this.withdraws = [];
+  this.fees = [];
 }
 
 Account.prototype.deposit = function(amount){
+  if(this.isSuspended){
+    return;
+  }
+
   this.balance += amount;
   this.deposits.push(amount);
+};
+
+Account.prototype.withdraw = function(amount){
+  if(this.isSuspended){
+    return;
+  }
+
+  this.balance -= amount;
+  this.withdraws.push(amount);
+
+  if(this.balance < 0){
+    this.balance -= 50;
+    this.fees.push(50);
+    if(this.fees.length >= 3){
+      this.isSuspended = true;
+    }
+  }
 };
 
 module.exports = Account;
--- test/unit/account.js
+++ test/unit/account.js
@@ -1,3 +1,4 @@
+/* jshint expr:true */
 /* global describe, it */
 
 'use strict';
@@ -14,8 +15,10 @@ describe('Account', function(){
       expect(sara.name).to.equal('Sara');
       expect(sara.type).to.equal('Savings');
       expect(sara.balance).to.equal(1500);
+      expect(sara.isSuspended).to.be.false;
       expect(sara.deposits).to.have.length(0);
       expect(sara.withdraws).to.have.length(0);
+      expect(sara.fees).to.have.length(0);
     });
   });
 
@@ -25,6 +28,37 @@ describe('Account', function(){
       sara.deposit(5000);
       expect(sara.balance).to.equal(6500);
       expect(sara.deposits).to.have.length(1);
+    });
+  });
+
+  describe('#withdraw', function(){
+    it('should withdraw money from account - normal', function(){
+      var sara = new Account(3, 'Sara', 1500, 'Savings');
+      sara.withdraw(500);
+      expect(sara.balance).to.equal(1000);
+      expect(sara.withdraws).to.have.length(1);
+    });
+
+    it('should withdraw money from account - overdraft', function(){
+      var sara = new Account(3, 'Sara', 1500, 'Savings');
+      sara.withdraw(2000);
+      expect(sara.balance).to.equal(-550);
+      expect(sara.withdraws).to.have.length(1);
+      expect(sara.fees).to.have.length(1);
+    });
+
+    it('should not withdraw money from account if 3 fees or greater', function(){
+      var sara = new Account(3, 'Sara', 1000, 'Savings');
+      sara.withdraw(2000);
+      sara.withdraw(2000);
+      sara.withdraw(2000);
+      sara.withdraw(2000);
+      sara.deposit(700);
+      expect(sara.balance).to.equal(-5150);
+      expect(sara.deposits).to.have.length(0);
+      expect(sara.withdraws).to.have.length(3);
+      expect(sara.fees).to.have.length(3);
+      expect(sara.isSuspended).to.be.true;
     });
   });
 });
